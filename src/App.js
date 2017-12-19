c7f7ca8b40e6b3c354f61e00a01b43701dc7085e
Gabi <gabriela@assembla.com>
2017-12-19 12:08:01 +0100

    Added first children property

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <h1>Hi, I am React App!</h1>
         <p>This is really working!</p>
         <Person name="Max" age="28" />
-        <Person name="Manu" age="29" />
+        <Person name="Manu" age="29">My hobbies are running and swimming.</Person>
       </div>
     );
     // return React.createElement('div', {className: 'App' }, React.createElement('h1', null, 'Hi, I am React App'));
