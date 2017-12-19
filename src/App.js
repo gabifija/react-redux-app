ba9884c092ce8e765bacf8dd4437fe055cf06949
Gabi <gabriela@assembla.com>
2017-12-19 14:59:12 +0100

    Added two way binding

--- src/App.js
+++ src/App.js
@@ -19,6 +19,16 @@ class App extends Component {
 				{ name: 'Anna', age: 24}
 			]
 		})
+	}
+
+	nameChangedHandler = (event) => {
+		this.setState({
+			persons: [
+				{ name: 'Max', age: 28},
+				{ name: event.target.value, age: 29},
+				{ name: 'Stephanie', age: 26}
+			]
+		})
 	}	
 
   	render() {
@@ -34,7 +44,8 @@ class App extends Component {
         		<Person 
         			name={this.state.persons[1].name}
         			age={this.state.persons[1].age}
-        			click={this.switchNameHandler.bind(this, 'Joanna')} />
+        			click={this.switchNameHandler.bind(this, 'Joanna')}
+        			changed={this.nameChangedHandler} />
         		<Person 
         			name={this.state.persons[2].name} 
         			age={this.state.persons[2].age}>My hobbies are swimming and running</Person>
