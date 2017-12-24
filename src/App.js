e5defad2a9863d7fca9590f623f214940a9151a4
Gabi <gabriela@assembla.com>
2017-12-24 10:19:35 +0100

    Added CSS modules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
-import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
 	state = {
@@ -58,11 +57,7 @@ class App extends Component {
   		font: 'inherit',
   		border: '1px solid blue',
   		padding: '8px',
-  		cursor: 'pointer',
-			':hover': {
-				backgroundColor: 'lightgreen',
-				color: 'black'
-			}
+  		cursor: 'pointer'
   	};
 
 		let persons = null;
@@ -82,36 +77,30 @@ class App extends Component {
   		);
 
 			style.backgroundColor = 'red';
-			style[':hover'] = {
-				backgroundColor: 'lightcoral',
-				color: 'black'
-			}
   	}
 
-		let classes = [];
+		let assignedClasses = [];
 
 		if (this.state.persons.length <= 2) {
-			classes.push('red'); // classes = ['red']
+			assignedClasses.push(classes.red); // classes = ['red']
 		}
 
 		if (this.state.persons.length <= 1) {
-			classes.push('bold'); // classes = ['red', 'bold']
+			assignedClasses.push(classes.bold); // classes = ['red', 'bold']
 		}
 
   	return (
-			<StyleRoot>
-				<div className="App">
-					<h1>Hi, I am React App!</h1>
-					<p className={classes.join(' ')}>This is really working!</p>
-					<button
-						style={style}
-						onClick={this.tooglePersonsHandler}>Toogle Persons</button>
-					{persons}
-				</div>
-			</StyleRoot>
+			<div className={classes.App}>
+				<h1>Hi, I am React App!</h1>
+				<p className={assignedClasses.join(' ')}>This is really working!</p>
+				<button
+					style={style}
+					onClick={this.tooglePersonsHandler}>Toogle Persons</button>
+				{persons}
+			</div>
   	);
     	// return React.createElement('div', {className: 'App' }, React.createElement('h1', null, 'Hi, I am React App'));
   }
 }
 
-export default Radium(App);
+export default App;
