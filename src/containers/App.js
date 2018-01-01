897d264120b86412ab9b05da68c1b347849d9c9d
Gabi <gabriela@assembla.com>
2018-01-01 15:36:49 +0100

    Added higher orger components

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,8 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import Aux from '../hoc/Aux';
+import withClass from '../hoc/withClass';
 
 class App extends PureComponent {
 	constructor(props) {
@@ -88,7 +89,7 @@ class App extends PureComponent {
   	}
 
   	return (
-			<WithClass classes={classes.App}>
+			<Aux>
 				<button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
 				<Cockpit
 					appTitle={this.props.title}
@@ -96,10 +97,10 @@ class App extends PureComponent {
 					persons={this.state.persons}
 					clicked={this.tooglePersonsHandler} />
 				{persons}
-			</WithClass>
+			</Aux>
   	);
     	// return React.createElement('div', {className: 'App' }, React.createElement('h1', null, 'Hi, I am React App'));
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
